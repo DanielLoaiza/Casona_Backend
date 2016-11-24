ac8e6d40416d470340736fe856877733b859edd3
Daniel Felipe Loaiza B <danielfloaiza@gmail.com>
2016-11-23 22:23:42 -0500

    created query with aggregation to group sections of food

--- src/api/food/index.js
+++ src/api/food/index.js
@@ -13,7 +13,8 @@ router.post('/', (req, res) => {
         let body = {
             name: req.body.name ? _g.validator.escape(_g.validator.trim(req.body.name)) : null,
             price: req.body.price ? _g.validator.escape(_g.validator.trim(req.body.price)) : null,
-            description: req.body.description ? _g.validator.escape(_g.validator.trim(req.body.description)) : null
+            description: req.body.description ? _g.validator.escape(_g.validator.trim(req.body.description)) : null,
+            section: req.body.section ? _g.validator.escape(_g.validator.trim(req.body.section)) : null
         }
         
         let areFieldsRight = body.name && body.price && body.description
@@ -22,14 +23,10 @@ router.post('/', (req, res) => {
             addFood(body)
                 .then((response) => {
                     res.json({
-                        success: true,
-                        data: {
-                            food: response
-                        }
+                        food: response
                     })
                 }).catch((e) => {
                 res.status(_g.constants.ERROR_CODES.HTTP_STATUS.INTERNAL_SERVER_ERROR).send({
-                    success: false,
                     error: e
                 })
             })
@@ -47,9 +44,7 @@ router.get('/', (req, res) => {
         getFood()
             .then((response) => {
                 res.json({
-                    data: {
-                        tables: response
-                    }
+                        sections: response
                 })
             }).catch((e) => {
             res.status(_g.constants.ERROR_CODES.HTTP_STATUS.INTERNAL_SERVER_ERROR).send({
--- src/lib/food/index.js
+++ src/lib/food/index.js
@@ -2,14 +2,19 @@ import {default as Food} from 'src/models/Food'
 
 
 export function addFood(foodData) {
-    return Food.create({name: foodData.number, price: foodData.price, description: foodData.description})
+    return Food.create({name: foodData.name, price: foodData.price, description: foodData.description, section: foodData.section})
         .catch((e) => {
             return Promise.reject(e)
         })
 }
 
 export function getFood() {
-    return Food.find({}).exec()
+    return Food.aggregate([{
+        $group: {
+            _id: '$section', //$section is the column name in collection
+            plates: { $push: {name:"$name", price:"$price", description:"$description", _id:"$_id"} }
+        }
+    }]).exec()
         .catch((e) => {
             return Promise.reject(e)
         })
